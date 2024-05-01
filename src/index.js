f0bdc4490486ac4c946be659a5dd68deffaafbee
KepoDes <86428587+Kepoo311@users.noreply.github.com>
2024-05-01 15:23:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -315,5 +315,5 @@ app.listen(port, () => {
                                                                                                                          
 `);
 
-  console.log(`App is succesful running on port http://127.0.0.1:${port} `);
+  console.log(`App is running on port http://127.0.0.1:${port} `);
 });
